a7c9fa7ec272fa85748769f5d75cb113fd797e38
Maruf Siddiqui <maruf.siddiqui@shore.com>
2018-03-08 21:40:57 +0100

    Add client index to hydrate app on client

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,5 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+ReactDOM.hydrate(<Home/>, document.getElementById('root'));
