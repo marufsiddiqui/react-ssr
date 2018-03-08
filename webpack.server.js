a4935b0571be029787f59421601c33f7e3d2be8d
Maruf Siddiqui <maruf.siddiqui@shore.com>
2018-03-08 21:54:25 +0100

    Setup webpack-node-externals

--- webpack.server.js
+++ webpack.server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpackNodeExternals = require('webpack-node-externals');
 const baseConfig = require('./webpack.base');
 
 const config = {
@@ -9,6 +10,7 @@ const config = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
