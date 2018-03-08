e7942821918968a04fd4cede94ff5711a616f55e
Maruf Siddiqui <maruf.siddiqui@shore.com>
2018-03-08 21:40:37 +0100

    Modify Home Component

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={() => console.log('hello')}>Click me</button>
+    </div>
+  )
 }
