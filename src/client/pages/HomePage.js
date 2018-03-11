c1dc37a9634fa259fb50ae8ea6990f434bbc6f00
Maruf Siddiqui <maruf.siddiqui@shore.com>
2018-03-11 14:41:16 +0100

    Move home and userlist as page component

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-export default function Home() {
+function Home() {
   return (
     <div>
       <h1>Home</h1>
       <button onClick={() => console.log('hello')}>Click me</button>
     </div>
   )
+}
+
+export default {
+  component: Home
 }
