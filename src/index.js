5528288c522bf84cebe4ee84f215fc3382a8b5bc
Maruf Siddiqui <maruf.siddiqui@shore.com>
2018-03-11 14:42:10 +0100

    Refactor code to use loadData to load the initial data in server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
+import routes from './client/Routes';
 
 const app = express();
 
@@ -9,8 +12,15 @@ app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   const store = createStore();
-  res.send(renderer(req, store));
-}); 
+
+  const promises = matchRoutes(routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
+
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  })
+});
 
 app.listen(3000, () => {
   console.log('App is running');
