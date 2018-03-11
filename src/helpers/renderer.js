a853871741a6b64e42028787e72da61d15574ffa
Maruf Siddiqui <maruf.siddiqui@shore.com>
2018-03-11 13:05:34 +0100

    Refactor both server and client routes

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Routes from '../client/Routes';
+import { renderRoutes } from 'react-router-config';
+
+import routes from '../client/Routes';
 
 export default (req, store) => {
   const content = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.path} context={{}}>
-        <Routes />
+        <div>{renderRoutes(routes)}</div>
       </StaticRouter>
     </Provider>
   );
