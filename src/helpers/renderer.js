3a36ce85b9753e600b4959237159fcecccebb483
Maruf Siddiqui <maruf.siddiqui@shore.com>
2018-03-11 12:17:17 +0100

    Modify server index to add redux store

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Routes from '../client/Routes';
 
-export default function renderer(req) {
+export default (req, store) => {
   const content = renderToString(
-    <StaticRouter location={req.path} context={{}}>
-      <Routes />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
   );
 
   return `
-<!doctype html>
-<html>
-  <head></head>
-  <body>
-    <div id="root">
-      ${content}
-    </div>
-    <script src="bundle.js"></script>
-  </body>
-</html>
+    <html>
+      <head></head>
+      <body>
+        <div id="root">${content}</div>
+        <script src="bundle.js"></script>
+      </body>
+    </html>
   `;
-}
+};
